df493fd257e1b74c2433d64f19bd676927015126
Henrik <henrik@thoroee.com>
2021-09-10 14:38:46 +0200

    Support css modules and SASS like syntax

--- next.config.js
+++ next.config.js
@@ -4,4 +4,28 @@ const { i18n } = require("./next-i18next.config.js")
 module.exports = {
   reactStrictMode: true,
   i18n,
+  webpack: (config) => {
+    const rules = config.module.rules
+      .find((rule) => typeof rule.oneOf === "object")
+      .oneOf.filter((rule) => Array.isArray(rule.use))
+
+    rules.forEach((rule) => {
+      rule.use.forEach((moduleLoader) => {
+        if (
+          moduleLoader.loader.includes("css-loader") &&
+          typeof moduleLoader.options.modules === "object"
+        ) {
+          moduleLoader.options = {
+            ...moduleLoader.options,
+            modules: {
+              ...moduleLoader.options.modules,
+              exportLocalsConvention: "camelCase", // https://github.com/webpack-contrib/css-loader#exportlocalsconvention
+            },
+          }
+        }
+      })
+    })
+
+    return config
+  },
 }
